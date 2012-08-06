69ccd0221fd198b3740576ec0560dd93998898a5
Ekiru <ekiru.t@gmail.com>
2012-08-06 12:47:21 +0300

    Version One- Project Setup

---
+++ pages/signup.php
@@ -0,0 +1,18 @@
+
+<div class="container">
+
+<div class="logo " align="center">
+<img src="assets/img/ideasafrica.logo.JPG" width="400"/>
+</div>
+<hr/>
+<div class="row-fluid offset3">
+<div class="span9 "  align="center">
+<!--<h1>Signup</h1>-->
+<button class="btn  span4"><h6>Signup with Twitter</h6></button> &nbsp;
+<button class="btn  span4"><h6>Signup with Facebook</h6></button>
+</div>
+<div class="span6"  align="center">
+</div>
+<div class="span4"></div>
+</div>
+</div>
---
+++ struct/header.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Ideas Africa</title>
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+<link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
