ee82b41b416d0b32b08bd1ea44242720f127df15
Ekiru <ekiru.t@gmail.com>
2012-09-18 17:00:55 +0300

    the dev

--- classes/class.adoket.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Adoket{
-	static public function save_data($data,$db_table,$db_field){
-		
-	}
-}
-?>
--- kernel/form/save.element.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include_once('../../includes.php');
-DBase::db_connect();
-
-//Save Data
-DBase::update_by_id($element['dbTable'],$element['dbFieldName'],$val,$user_id);
-
-?>
